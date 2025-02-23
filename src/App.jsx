f2b9f5d5e11cc5f8df2fa2afe6d6fd934cb468c9
Mihir Vyas <mihirv50@gmail.com>
2025-02-23 16:59:51 +0530

    Set up complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "./Components/sidebar/Sidebar";
+import Hero from "./Components/main/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="w-screen h-screen bg-red-100 flex">
+        <Sidebar />
+        <Hero/>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[15%] h-full bg-red-400'>Sidebar</div>
+  )
+}
+
+export default Sidebar
